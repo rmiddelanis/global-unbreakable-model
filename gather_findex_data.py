4447dc9b4cefc1fd4291e3736fff67fd490d3205
Robin Middelanis <r.middelanis@gmail.com>
2024-01-17 14:31:01 +0100

    added previous FINDEX data, reshaped findex csv output

--- gather_findex_data.py
+++ gather_findex_data.py
@@ -1,39 +1,71 @@
+import numpy as np
 import pandas as pd
 
 
-def gather_findex_data(findex_data_path, axfin_outpath=None):
+def gather_findex_data(findex_data_paths: dict, axfin_outpath=None):
     """
-    This function gathers and processes data from the FINDEX dataset.
+    This function gathers and processes data from the FINDEX datasets.
+
+    Data can be obtained from
+    https://doi.org/10.48529/JQ97-AJ70 (FINDEX 2021)
+    https://doi.org/10.48529/d3cf-fj47 (FINDEX 2017)
+    https://doi.org/10.48529/9j25-hr41 (FINDEX 2014)
+    https://doi.org/10.48529/wnvd-y919 (FINDEX 2011)
 
     Parameters:
-    findex_data_path (str): The path to the 2021 FINDEX data file. File can be obtained from https://doi.org/10.48529/JQ97-AJ70
+    findex_data_path (dict): Values contain the paths to the FINDEX data file with the year as dict key.
     axfin_outpath (str, optional): The path to the output file. If provided, the result will be saved to this file.
 
     Returns:
     None
     """
+    findex_datasets = []
+    for year, findex_data_path in findex_data_paths.items():
+        # Load the data from the provided path, selecting only the necessary columns
+        findex_data = pd.read_csv(findex_data_path, encoding='latin1')
 
-    # Load the data from the provided path, selecting only the necessary columns
-    findex_data = pd.read_csv(findex_data_path, encoding='latin1')[['economy', 'economycode', 'inc_q', 'fin17a', 'wgt']]
+        # Rename the columns for better readability
+        if year in [2017, 2021]:
+            varname = 'fin17a'
+        elif year == 2014:
+            varname = 'q18a'
+        elif year == 2011:
+            varname = 'q13a'
+            findex_data.rename({'ecnmycode': 'economycode'}, axis=1, inplace=True)
+        else:
+            raise ValueError('Unknown FINDEX year: {}'.format(np.unique(findex_data.year).item()))
 
-    # Rename the columns for better readability
-    findex_data.rename({'economy': 'country', 'economycode': 'iso3', 'inc_q': 'income_quintile', 'fin17a': 'axfin'},
-                       axis=1, inplace=True)
+        if 'year' not in findex_data.columns:
+            findex_data['year'] = year
 
-    # Set the index of the DataFrame to be a combination of country, iso3 code, and income quintile
-    findex_data.set_index(['country', 'iso3', 'income_quintile'], inplace=True)
+        findex_data.rename({'economy': 'country', 'inc_q': 'income_cat', varname: 'axfin'},
+                           axis=1, inplace=True)
 
-    # Replace the values in the 'axfin' column based on the FINDEX 2021 codebook:
-    # 1 if the respondent saved or set aside any money (value=1)
-    # 0 if no (=2), don’t know (=3), or refused to answer (=4)
-    findex_data.axfin.replace({1: 1, 2: 0, 3: 0, 4: 0}, inplace=True)
+        # findex_data = findex_data[['country', 'income_cat', 'axfin', 'wgt', 'year']].dropna()
+        findex_data = findex_data[['country', 'income_cat', 'axfin', 'wgt', 'year']].fillna(0)
+        findex_data = findex_data.astype({'country': 'str', 'income_cat': 'str', 'axfin': 'float',
+                                          'wgt': 'float', 'year': 'int'})
+        findex_data.income_cat = findex_data.income_cat.apply(lambda x: 'q{}'.format(x))
 
-    # Calculate the result by multiplying all columns (prod), grouping by country, iso3 code, and income quintile,
+        # Set the index of the DataFrame to be a combination of country, and income quintile
+        findex_data.set_index(['country', 'year', 'income_cat'], inplace=True)
+
+        # Replace the values in the 'axfin' column based on the FINDEX 2021 codebook:
+        # 1 if the respondent saved or set aside any money (value=1)
+        # 0 if no (=2), don’t know (=3), or refused to answer (=4)
+        findex_data.axfin.replace({1: 1, 2: 0, 3: 0, 4: 0}, inplace=True)
+
+        findex_datasets.append(findex_data)
+
+    # Concatenate the datasets
+    findex_data = pd.concat(findex_datasets)
+
+    # Calculate the result by multiplying all columns (prod), grouping by country, and income quintile,
     # summing the groups, and then dividing by the sum of the 'wgt' column for each group
-    res = (findex_data.prod(axis=1).groupby(['country', 'iso3', 'income_quintile']).sum()
-           / findex_data.wgt.groupby(['country', 'iso3', 'income_quintile']).sum())
-    res.name = 'axfin'
+    findex_data = (findex_data.prod(axis=1).groupby(['country', 'year', 'income_cat']).sum()
+                   / findex_data.wgt.groupby(['country', 'year', 'income_cat']).sum())
+    findex_data.name = 'axfin'
 
     # If an output path is provided, save the result to a CSV file at that path
     if axfin_outpath is not None:
-        res.to_csv(axfin_outpath)
+        findex_data.to_csv(axfin_outpath)
