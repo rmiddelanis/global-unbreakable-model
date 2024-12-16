31f72fe5aa6f40638ab1e39f10b3aa768f8f2ac0
rmiddelanis <r.middelanis@gmail.com>
2024-12-16 11:24:36 +0100

    updated policies

--- apply_policy.py
+++ apply_policy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def apply_policy(macro_, cat_info_, hazard_ratios_, policy_name=None, policy_opt=None, verbose=True):
+def apply_policy(macro_, cat_info_, hazard_ratios_, policy_name=None, policy_opt=None):
     """
     Chooses a policy by name, applies it to macro, cat_info, and/or hazard_ratios, and returns new values as well
     as a policy description
@@ -28,7 +28,7 @@ def apply_policy(macro_, cat_info_, hazard_ratios_, policy_name=None, policy_opt
         fa_poor_new = hazard_ratios.loc[poor_sel, 'fa'] - policy_opt * total_exposure / hazard_ratios.loc[poor_sel, 'n']
         fa_poor_new = fa_poor_new.fillna(0).clip(lower=0)
         hazard_ratios.fa.update(fa_poor_new)
-        policy_name = 'reduce_poor_exposure_{}'.format(int(policy_opt * 100))
+        policy_name = 'reduce_poor_exposure_{}'.format(policy_opt)
     elif policy_name == 'reduce_rich_exposure':
         if policy_opt is None:
             policy_opt = 0.05
@@ -41,7 +41,7 @@ def apply_policy(macro_, cat_info_, hazard_ratios_, policy_name=None, policy_opt
         fa_rich_new = hazard_ratios.loc[rich_sel, 'fa'] - policy_opt * total_exposure / hazard_ratios.loc[rich_sel, 'n']
         fa_rich_new = fa_rich_new.fillna(0).clip(lower=0)
         hazard_ratios.fa.update(fa_rich_new)
-        policy_name = 'reduce_rich_exposure_{}'.format(int(policy_opt * 100))
+        policy_name = 'reduce_rich_exposure_{}'.format(policy_opt)
     elif policy_name == 'reduce_nonpoor_exposure':
         if policy_opt is None:
             policy_opt = 0.05
@@ -54,21 +54,40 @@ def apply_policy(macro_, cat_info_, hazard_ratios_, policy_name=None, policy_opt
         fa_nonpoor_new = hazard_ratios.loc[nonpoor_sel, 'fa'] - 1 / 4 * policy_opt * total_exposure / hazard_ratios.loc[nonpoor_sel, 'n']
         fa_nonpoor_new = fa_nonpoor_new.fillna(0).clip(lower=0)
         hazard_ratios.fa.update(fa_nonpoor_new)
-        policy_name = 'reduce_nonpoor_exposure_{}'.format(int(policy_opt * 100))
+        policy_name = 'reduce_nonpoor_exposure_{}'.format(policy_opt)
     elif policy_name == 'reduce_total_exposure':
         if policy_opt is None:
             policy_opt = 0.05
         desc = f"Uniformly reduce exposure by {policy_opt * 100}%"
         hazard_ratios.fa = hazard_ratios.fa * (1 - policy_opt)
-        policy_name = 'reduce_total_exposure_{}'.format(int(policy_opt * 100))
+        policy_name = 'reduce_total_exposure_{}'.format(policy_opt)
+    elif policy_name == 'reduce_poor_vulnerability':
+        if policy_opt is None:
+            policy_opt = 0.05
+        desc = f"Reduce poor vulnerability s.th. country vulnerability decreases by {policy_opt * 100}%"
+        hazard_ratios = pd.merge(hazard_ratios, cat_info.n, left_index=True, right_index=True)
+        hazard_ratios = hazard_ratios.reorder_levels(hazard_ratios_.index.names)
+        total_vulnerability = (hazard_ratios[['v', 'n']].product(axis=1).groupby(['iso3', 'hazard', 'rp']).sum() /
+                          hazard_ratios.n.groupby(['iso3', 'hazard', 'rp']).sum())
+        poor_sel = hazard_ratios.index.get_level_values('income_cat').isin(['q1'])
+        v_poor_new = hazard_ratios.loc[poor_sel, 'v'] - policy_opt * total_vulnerability / hazard_ratios.loc[poor_sel, 'n']
+        v_poor_new = v_poor_new.fillna(0).clip(lower=0)
+        hazard_ratios.v.update(v_poor_new)
+        policy_name = 'reduce_poor_vulnerability_{}'.format(policy_opt)
+    elif policy_name == 'reduce_total_vulnerability':
+        if policy_opt is None:
+            policy_opt = 0.05
+        desc = f"Uniformly reduce vulnerability by {policy_opt * 100}%"
+        hazard_ratios.v = hazard_ratios.v * (1 - policy_opt)
+        policy_name = 'reduce_total_vulnerability_{}'.format(policy_opt)
     elif policy_name == 'no_liquidity':
         desc = "No liquidity"
         cat_info['liquidity'] = 0
     elif policy_name == 'reduce_ew':
-        desc = "Reduce early warning to {policy_opt * 100}%"
+        desc = f"Reduce early warning to {policy_opt * 100}%"
         macro.ew = macro.ew * policy_opt
         hazard_ratios.ew = hazard_ratios.ew * policy_opt
-        policy_name = 'reduce_ew_{}'.format(int(policy_opt * 100))
+        policy_name = 'reduce_ew_{}'.format(policy_opt)
     elif policy_name == 'increase_ew_to_max':
         desc = "Increase early warning to maximum"
         macro.ew = macro.ew.max()
